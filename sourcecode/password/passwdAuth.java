aa05cedb57d75ca0be3b1bae601a18d3d4ce0303
RyanRaffoul <72235540+RyanRaffoul@users.noreply.github.com>
2022-03-30 19:24:02 -0400

    Update passwdAuth.java

--- sourcecode/password/passwdAuth.java
+++ sourcecode/password/passwdAuth.java
@@ -1,46 +1,50 @@
-import java.io.*; 
-import java.util.*; 
-class passwdAuth 
-	{ 
-	 public static void main(String args[]) 
-	 { 
-		 try 
-		 { 
-			 String logFileName = args[0]; 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+
+public class passwdAuth {
+
+	public static void main(String[] args) {
+		try { 
+			 String logFileName = args[0];
+			 int checkFile = logFileName.lastIndexOf('.');
+			 if(checkFile == -1) {
+				 throw new Exception("Incorrect File Format");
+			 }
 			 File f = new File(logFileName); 
 			 boolean access_granted = false; 
 			 String password = ""; 
-			 int integer = 5; // Comment for poor programming style
-			 
-			 if (args.length == 2) 
-			 { 
+			 int integer = 0;
+		 
+			 if(args.length == 2){ 
 				 password = password + args[1]; // Comment for Empty Password Initialization Vulnerability 
 				 System.out.println("Checking command-line password"); 
-				 if (password.equals("3dTAqb.7")){
-				 	access_granted = true; 
-			 	 	System.out.println("Password matches.");  
+				 String password1 = "3dTAqb.7";
+				 if (password.equals(password1)){
+					 access_granted = true; 
+					 System.out.println("Password matches.");  
 				 }else{
-				        System.out.println("Command-line password does not match"); 
+					 System.out.println("Command-line password does not match"); 
 				 } 
-			 }//end if 
-			 
-			 if (access_granted) 
-			 { 
+			 }else{
+				 throw new Exception("Incorrect Arguments Given");
+			 }
+		 
+			 if(access_granted){ 
 				 System.out.println("Access granted!"); 
 				 PrintWriter out = new PrintWriter(new 
-				FileOutputStream(f, true)); 
+				 FileOutputStream(f, true)); 
 				 out.println(); 
 				 out.print("Updated..."); 
 				 out.println(); 
 				 out.flush(); 
 				 out.close(); 
 			 }//end if 
-		 
-		 }//end try 
-		 catch (Exception e) 
-		 { 
-			 System.out.println("an error has occured."); 
-			 e.printStackTrace(); //Comment for system leak vulnerability
-		 } 
-	 }//end main 
-}//end class
+	 
+		}catch (Exception e) { 
+			System.out.println("an error has occured."); 
+			e.printStackTrace();
+		} 
+	}
+
+}
